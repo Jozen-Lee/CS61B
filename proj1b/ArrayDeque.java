1391e106419150bdc6ac15a877d5c4aabfb10ac9
Jozen <2250017028@qq.com>
2020-06-21 16:31:49 +0800

    pro1b completed

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,209 @@
+public class ArrayDeque<T> implements Deque<T>
+{
+    /**
+     * the initial size
+     */
+    private final int INITIAL_SIZE = 8;
+    /**
+     *The items of the Array Deque
+     */
+    private T[] items;
+
+    /**
+     * The size of the Array Deque
+     */
+    private int size;
+
+    /**
+     * The first and last item of the deque
+     */
+    private int nextFirst;
+    private int nextLast;
+
+    /**
+     * Creat an empty Array Deque
+     */
+    public ArrayDeque()
+    {
+        items = (T[]) new Object[INITIAL_SIZE];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    private int minusOne(int index)
+    {
+        return Math.floorMod(index-1, items.length);
+    }
+
+    private int plusOne(int index)
+    {
+        return Math.floorMod(index+1, items.length);
+    }
+
+    private int plusOne(int index, int length) {
+        return Math.floorMod(index+1, length);
+    }
+
+    /**
+     * Resize the underlying array to the target capacity
+     */
+    private void resize()
+    {
+        if(size == items.length)
+        {
+            expand();
+        }
+        if(size < items.length / 4 && items.length > 8)
+        {
+            reduce();
+        }
+    }
+
+    private void expand()
+    {
+        resizeHelper(items.length * 2);
+    }
+
+    private  void reduce()
+    {
+        resizeHelper(items.length / 2);
+    }
+
+    private  void resizeHelper(int capacity)
+    {
+        T[] temp = items;
+        int begin = plusOne(nextFirst);
+        int end = minusOne(nextLast);
+        items = (T[]) new Object[capacity];
+        nextFirst = 0;
+        nextLast = 1;
+        for(int i = begin; i != end; i = plusOne(i, temp.length))
+        {
+            items[nextLast] = temp[i];
+            nextLast = plusOne(nextLast);
+        }
+        items[nextLast] = temp[end];
+        nextLast = plusOne(nextLast);
+    }
+
+    /**
+     * Add an item of type T to the back of the deque
+     * @param item
+     */
+    public void addLast(T item)
+    {
+        resize();
+        items[nextLast] = item;
+        nextLast = plusOne(nextLast);
+        size ++;
+    }
+
+    /**
+     * 返回items中最后一个元素
+     * @return item[last]
+     */
+    private T getlast()
+    {
+        return items[minusOne(nextLast)];
+    }
+
+    /**
+     * 删除并返回items中最后一个元素
+     * @return items[last]
+     */
+    public T removeLast()
+    {
+        if(isEmpty()) return null;
+        resize();
+        T res = getlast();
+        nextLast = minusOne(nextLast);
+        items[nextLast] = null;
+        size --;
+        return res;
+    }
+
+    /**
+     * Add an item of type T to the front of the deque
+     * @param item
+     */
+    public void addFirst(T item)
+    {
+        resize();
+        items[nextFirst] = item;
+        nextFirst = minusOne(nextFirst);
+        size ++;
+    }
+
+    /**
+     * 返回items中第一个元素
+     * @return item[first]
+     */
+    private T getFirst()
+    {
+        return items[plusOne(nextFirst)];
+    }
+
+    /**
+     * 删除并返回items中第一个元素
+     * @return item[first]
+     */
+    public T removeFirst()
+    {
+        if(isEmpty()) return null;
+        resize();
+        T res = getFirst();
+        nextFirst = plusOne(nextFirst);
+        items[nextFirst] = null;
+        size --;
+        return res;
+    }
+
+    /**
+     * Check if the deque is empty.
+     * @return bool
+     */
+    public boolean isEmpty()
+    {
+        return (size == 0 ? true : false);
+    }
+
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, return null.
+     * Must not alter the deque!
+     * @param index
+     * @return  items[index] or null
+     */
+    public T get(int index)
+    {
+        if(index >= size || index < 0) return  null;
+        else
+        {
+            index = Math.floorMod(plusOne(nextFirst) + index, items.length);
+            return items[index];
+        }
+    }
+
+    /**
+     * 打印deque序列.
+     */
+    public void printDeque()
+    {
+        for(int index = plusOne(nextFirst); index != nextLast; index = plusOne(index))
+        {
+            System.out.println(items[index]);
+            System.out.println(' ');
+        }
+        System.out.println('\n');
+    }
+
+    /**
+     * Return the size of Array Deque.
+     * @return size
+     */
+    public int size()
+    {
+        return size;
+    }
+}
---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,123 @@
+
+public class LinkedListDeque<T> implements Deque<T>
+{
+    public int size;
+    private Node First;
+    private Node Last;
+
+    private class Node<T>
+    {
+        public T item;
+        public Node prev;
+        public Node next;
+
+        public Node(T i, Node p, Node n)
+        {
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+
+    public LinkedListDeque()
+    {
+        size = 0;
+        First = new Node(null,null,null);
+        Last = new Node(null,null,null);
+        First.next = Last;
+        Last.prev = First;
+    }
+
+    public void addFirst(T item)
+    {
+        Node node = new Node(item, First, First.next);
+        First.next = node;
+        First.next.next.prev = node;
+        size ++;
+    }
+
+    public void addLast(T item)
+    {
+        Node node = new Node(item, Last.prev, Last);
+        Last.prev = node;
+        Last.prev.prev.next = node;
+        size ++;
+    }
+
+    public boolean isEmpty()
+    {
+        if(size == 0) return true;
+        else return false;
+    }
+
+    public int size()
+    {
+        return size;
+    }
+
+    public void printDeque()
+    {
+        Node node = First.next;
+        while (node != Last)
+        {
+            System.out.println(node.item);
+            System.out.println(' ');
+            node = node.next;
+        }
+        System.out.println('\n');
+    }
+
+    public T removeFirst()
+    {
+        if(isEmpty()) return null;
+        else
+        {
+            Node node = First.next;
+            First.next = node.next;
+            First.next.prev = First;
+            size --;
+            return  (T) node.item;
+        }
+    }
+
+    public T removeLast()
+    {
+        if(isEmpty()) return null;
+        else
+        {
+            Node node = Last.prev;
+            Last.prev = node.prev;
+            Last.prev.next = Last;
+            size --;
+            return  (T) node.item;
+        }
+    }
+
+    public T get(int index)
+    {
+        if(index > size()) return null;
+        Node node = First.next;
+        while (index != 0)
+        {
+            node = node.next;
+            index --;
+        }
+        return (T) node.item;
+    }
+
+    private T getRecursiveHelper(Node node,int index)
+    {
+        if(index == 0) return(T) node.item;
+        else return (T) getRecursiveHelper(node.next, (index-1));
+    }
+
+    public T getRecursive(int index)
+    {
+        if(index > size()) return null;
+        else
+        {
+            Node node = First.next;
+            return getRecursiveHelper(node, index);
+        }
+    }
+}
