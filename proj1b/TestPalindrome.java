ef2da68403f41bf8a4ef89d7113020d22915d637
Jozen <2250017028@qq.com>
2020-06-21 16:27:35 +0800

    pro1b completed

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
@@ -14,5 +14,33 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    } /**Uncomment this class once you've created your Palindrome class. */
+
+    @Test
+    public void testIsPalindrome()
+    {
+        assertTrue(palindrome.isPalindrome("abccba"));
+        assertTrue(palindrome.isPalindrome("noon"));
+        assertFalse(palindrome.isPalindrome("baby"));
+        assertFalse(palindrome.isPalindrome("Jozen"));
+    }
+
+    @Test
+    public void testPalindrome()
+    {
+        OffByOne ob = new OffByOne();
+        assertTrue(palindrome.isPalindrome("flake", ob));
+        assertFalse(palindrome.isPalindrome("aaabbbaaa", ob));
+        assertTrue(palindrome.isPalindrome("", ob));
+        assertTrue(palindrome.isPalindrome("&", ob));
+    }
+
+    @Test
+    public void testPalindromeByN() {
+        OffByN offByN = new OffByN(6);
+        assertTrue(palindrome.isPalindrome("gmdjgm", offByN));
+        assertFalse(palindrome.isPalindrome("aaabbbaa", offByN));
+        assertTrue(palindrome.isPalindrome("", offByN));
+        assertTrue(palindrome.isPalindrome("&", offByN));
+    }
 }
